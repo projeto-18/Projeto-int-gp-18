d98c4a47352a8c2f2b96914c148a1327cf388bea
FCamposDev <fcamposdevoficial@gmail.com>
2023-04-29 16:43:45 -0300

    Atualização de condigos (Index-Cadastro)

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -1,23 +1,59 @@
-let btn = document.querySelector('#verSenha')
-let btnConfirm = document.querySelector('#verConfirmSenha')
-btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#password')
-  
-  if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
-    
-  } else {
-    inputSenha.setAttribute('type', 'password')
-  }
+const cpf = document.querySelector("#cpf")
+
+cpf.addEventListener("keyup", event => {
+    let start = cpf.selectionStart //posição inicial do cursor
+    let end = cpf.selectionEnd //posição final do cursor
+    //se as posições iniciazl e final do cursor forem as mesma, isso significa que não há faixa de seleção
+    if(start == end) { //esta condicional impede a função chamada quando o usuário faz uma seleção de faixa na entrada EX:. teclas (Ctrl + A)
+        formMask("___.___.___-__", "_", event, start);
+    }
 })
 
-btnConfirm.addEventListener('click', ()=>{
-  let inputConfirmSenha = document.querySelector('#confirm_password')
-  
-  if(inputConfirmSenha.getAttribute('type') == 'password'){
-    inputConfirmSenha.setAttribute('type', 'text')
-  } else {
-    inputConfirmSenha.setAttribute('type', 'password')
+function formMask(mask, char, event, cursor) {
     
+    const vetMask = mask.split("") //transforme a máscara em vetor para usar funções específicas, como filter()
+    const onlyNumbers = cpf.value.split("").filter(value => !isNaN(value) && value != " ")
+    const key = event.keyCode || event.which
+    const backspaceAndArrowKeys = [8, 37, 38, 39, 40] //código backspace e teclas de seta
+    const clickedOnTheBackspaceOrArrowsKeys = backspaceAndArrowKeys.indexOf(key) >= 0
+    const charNoMod = [".", "-"] //caracteres que não mudam
+    const cursorIsCloseToCharNoMod = charNoMod.indexOf(vetMask[cursor]) >= 0
+
+    onlyNumbers.forEach( (value) => vetMask.splice(vetMask.indexOf(char), 1, value)) //mude '#' para números
+
+    cpf.value = vetMask.join("")
+
+    if(!clickedOnTheBackspaceOrArrowsKeys && cursorIsCloseToCharNoMod) { //incrementar o cursor se estiver perto de caracteres que não mudam
+        cpf.setSelectionRange(cursor+1, cursor+1)
+    } else {
+        cpf.setSelectionRange(cursor, cursor)
+    }
+}
+
+/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
+
+function mostrarSenhaCadastro(){
+  var inputPass = document.getElementById('password')
+  var btnShowPass = document.getElementById('btn-senhaCadaastro')
+
+  if(inputPass.type === 'password'){
+    inputPass.setAttribute('type', 'text')
+    btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+  } else {
+        inputPass.setAttribute('type', 'password')
+    btnShowPass.classList.replace( 'bi-eye-slash-fill','bi-eye-fill')
   }
-})
+}
+
+function mostrarSenhaConfirmaCadastro(){
+  var inputPass = document.getElementById('confirm_password')
+  var btnShowPass = document.getElementById('btn-senhaconfirmaCadaastro')
+
+  if(inputPass.type === 'password'){
+    inputPass.setAttribute('type', 'text')
+    btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+  } else {
+        inputPass.setAttribute('type', 'password')
+    btnShowPass.classList.replace( 'bi-eye-slash-fill','bi-eye-fill')
+  }
+}
---
+++ src/php/index.php
@@ -0,0 +1,58 @@
+<?php
+include_once('database.php');
+
+$query = "SELECT * FROM tbl_usuario";
+$result = mysqli_query($connection, $query);
+
+if (!$result) {
+    die("Query failed: " . mysqli_error($connection));
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['name'] . "<br>";
+}
+?>
+
+
+
+<?php 
+include('conexao.php');
+
+if(isset($_POST['email']) || isset($_POST['senha'])) {
+
+    if(strlen($_POST['email']) == 0) {
+        echo "Preencha seu e-mail";
+    } else if(strlen($_POST['senha']) == 0) {
+        echo "Preencha sua senha";
+    } else {
+
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $senha = $mysqli->real_escape_string($_POST['senha']);
+
+        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+        $quantidade = $sql_query->num_rows;
+
+        if($quantidade == 1) {
+            
+            $usuario = $sql_query->fetch_assoc();
+
+            if(!isset($_SESSION)) {
+                session_start();
+            }
+
+            $_SESSION['id'] = $usuario['id'];
+            $_SESSION['nome'] = $usuario['nome'];
+
+            header("Location: painel.php");
+
+        } else {
+            echo "Falha ao logar! E-mail ou senha incorretos";
+        }
+
+    }
+
+}
+?>
+
